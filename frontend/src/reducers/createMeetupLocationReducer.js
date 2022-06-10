35627ccc36fb4bb7549f22d4c8eb01398f343965
shiyuc <shiyuc@students.cs.ubc.ca>
2022-06-10 16:48:27 -0700

    include new reducers

---
+++ frontend/src/reducers/createMeetupLocationReducer.js
@@ -0,0 +1,15 @@
+const createMeetupLocationReducer = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_LOCATION":
+            if (!state.some(e => e.place_id === action.newPlace.place_id)) {
+                return [...state, action.newPlace];
+            }
+            return state;
+        case "DELETE_LOCATION":
+            return state.filter((e) => { return e.place_id !== action.id });
+        default:
+            return state;
+    }
+};
+
+export default createMeetupLocationReducer;
