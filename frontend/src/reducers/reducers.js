bb3a68008af865b8dbedefbd4e08fee28003d4e7
guorunhe <guorunhe@students.cs.ubc.ca>
2022-06-09 21:05:29 -0700

    fix location add duplicate places

--- frontend/src/reducers/reducers.js
+++ frontend/src/reducers/reducers.js
@@ -1,7 +1,10 @@
 const reducers = (state = [], action) => {
     switch (action.type) {
         case "ADD_R":
-            return [...state, action.newPlace];
+            if (!state.some(e=> e.place_id === action.newPlace.place_id)){
+                return [...state, action.newPlace];
+            }
+            return state;
         case "DEL_R":
             return state.filter((e) => { return e.place_id !== action.id });
         default:
