088dc43eaa7e80d6fa7b4ac3cddcdad5b1a7240b
guorunhe <guorunhe@students.cs.ubc.ca>
2022-06-09 20:29:36 -0700

    fix hook warning

--- frontend/src/MeetupLocation.js
+++ frontend/src/MeetupLocation.js
@@ -6,39 +6,43 @@ import Place from './Place.js';
 
 let script;
 let autoComplete;
+let dispatch;
 const k1 = "AIzaSyDHH_p0fbbZSRyr";
 const k2 = "HqvLAc5WcM7Ic26ypP4";
 const k = k1 + k2;
 
+const loadScript = (url, callback) => {
+    script.type = "text/javascript";
+    if (script.readyState) {
+        script.onreadystatechange = function () {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {
+        script.onload = () => callback();
+    }
+    script.src = url;
+};
+
+function handleScriptLoad(updateQuery, autoCompleteRef) {
+    autoComplete = new window.google.maps.places.Autocomplete(autoCompleteRef.current, {});
+    autoComplete.setFields(["address_component", "adr_address", "alt_id", "formatted_address", "icon", "name", "place_id", "type", "url"]);
+    autoComplete.addListener("place_changed", () => handlePlaceSelect(updateQuery));
+}
+
+async function handlePlaceSelect(updateQuery) {
+    const addressObject = autoComplete.getPlace();
+    updateQuery("");
+    console.log(addressObject);
+    if (addressObject.formatted_address !== undefined) {
+        dispatch(addR(addressObject));
+    }
+}
+
 export default function MeetupLocation() {
-    const dispatch = useDispatch();
-    const loadScript = (url, callback) => {
-        script.type = "text/javascript";
-        if (script.readyState) {
-            script.onreadystatechange = function () {
-                if (script.readyState === "loaded" || script.readyState === "complete") {
-                    script.onreadystatechange = null;
-                    callback();
-                }
-            };
-        } else {
-            script.onload = () => callback();
-        }
-        script.src = url;
-    };
-    function handleScriptLoad(updateQuery, autoCompleteRef) {
-        autoComplete = new window.google.maps.places.Autocomplete(autoCompleteRef.current, {});
-        autoComplete.setFields(["address_component", "adr_address", "alt_id", "formatted_address", "icon", "name", "place_id", "type", "url"]);
-        autoComplete.addListener("place_changed", () => handlePlaceSelect(updateQuery));
-    }
-    async function handlePlaceSelect(updateQuery) {
-        const addressObject = autoComplete.getPlace();
-        updateQuery("");
-        console.log(addressObject);
-        if (addressObject.formatted_address !== undefined) {
-            dispatch(addR(addressObject));
-        }
-    }
+    dispatch = useDispatch();
     const [query, setQuery] = useState("");
     const autoCompleteRef = useRef(null);
     script = document.createElement("script");
