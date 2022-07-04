3ac4dd8e246111e0c067b27d740de1a39ea75f7b
guorunhe <56808163+guorunhe-design@users.noreply.github.com>
2022-07-03 21:50:19 -0700

    User (#68)
    * change id to _id
    
    * user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,23 +3,26 @@ var router = express.Router();
 var jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 var verifyJWT = require('../middlewares/auth');
+const queries = require('../model/queries');
 require('dotenv').config()
 
 /** Schema of user include:
  *  username (unique key for user), password (encryption needed?), email address...
  */
 
-let users = [
-    {
-        email: "a",
-        username: "a",
-        password: "$2a$10$FKPFoOsz.td5FTaOs5kr7eot3WGjEo3pFP2e7eOBSW5Dw0Mry3yF6" // hash for b
-    }
-]
+// let users = [
+//     {
+//         email: "a",
+//         username: "a",
+//         password: "$2a$10$FKPFoOsz.td5FTaOs5kr7eot3WGjEo3pFP2e7eOBSW5Dw0Mry3yF6" // hash for b
+//     }
+// ]
 
 /* get all users. */
 router.get('/', function (req, res, next) {
-    return res.send(users);
+    queries.getUsers({}).then(users => {
+        return res.send(users);
+    });
 });
 
 
@@ -31,29 +34,37 @@ router.post('/register', function (req, res, next) {
     // TODO: verify user information (password strong? email valid?)
 
     // check duplicate email and username
-    const takenUserName = users.find(u => u.username === user.username)
-    const takenEmail = users.find(u => u.username === user.username)
 
-    if (takenUserName || takenEmail) {
-        res.json({
-            status: "error",
-            message: "Duplicate username of email",
-            code: 400
-        })
-    }
-
-    // encrypt password
-    user.password = bcrypt.hash(user.password, 10).then(
-        (password) => {
-            user.password = password
-            users.push(user)
-            return res.send({
-                status: "success",
-                user: user.username
-            })
-        }
-    )
-
+    // const takenUserName = users.find(u => u.username === user.username)
+    // const takenEmail = users.find(u => u.username === user.username)
+    let takenUserName1;
+    let takenEmail1;
+    queries.getUserByUsername(user.username).then(takenUserName => {
+        // console.log(takenUserName)
+        // console.log(1111)
+        takenUserName1 = takenUserName;
+        queries.getUserByEmail(user.email).then(takenEmail => {
+            // console.log(takenEmail)
+            // console.log(2222)
+            takenEmail1 = takenEmail;
+            if (takenUserName1 !== null || takenEmail1 !== null) {
+                // console.log(3333)
+                return res.json({
+                    status: "error",
+                    message: "Username or Email already taken"
+                })
+            } else {
+                // console.log(4444)
+                // encrypt password
+                user.password = bcrypt.hash(user.password, 10).then((password) => {
+                    user.password = password;
+                    queries.addUser(user).then(user => {
+                        return res.status(200).send(user);
+                    })
+                })
+            }
+        });
+    });
 });
 
 
@@ -63,17 +74,15 @@ router.post('/login', (req, res) => {
 
     // console.log(user)
 
-    const matchStoredUser = users.find(u => u.email === user.email)
-    // console.log("matchStoredUser", matchStoredUser)
-    if (!matchStoredUser) {
-        return res.status(404).send(new Error("Email does not exist"))
-
-    }
-
-    // console.log("matchStoredUser", matchStoredUser)
-
-    bcrypt.compare(user.password, matchStoredUser.password)
-        .then(passwordCorrect => {
+    // const matchStoredUser = users.find(u => u.email === user.email)
+    let matchStoredUser;
+    queries.getUserByEmail(user.email).then(result => {
+        matchStoredUser = result;
+        // console.log("matchStoredUser", matchStoredUser)
+        if (!result) {
+            return res.status(404).send(new Error("Email does not exist"))
+        }
+        bcrypt.compare(user.password, matchStoredUser.password).then(passwordCorrect => {
             // console.log(passwordCorrect)
             if (passwordCorrect) {
                 let payload = {
@@ -98,13 +107,11 @@ router.post('/login', (req, res) => {
 
                     }
                 )
-
-
             } else {
                 return res.status(404).send(new Error("Incorrect password"))
             }
-        })
-
+        });
+    });
 })
 
 // access information about a particular user
