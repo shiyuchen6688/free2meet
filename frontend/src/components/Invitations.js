8cc963a50bbf64cb299e7424e36d2b8dc57aabf5
guorunhe <grh20010102@gmail.com>
2022-07-07 00:56:50 -0700

    add more

--- frontend/src/components/Invitations.js
+++ frontend/src/components/Invitations.js
@@ -1,16 +1,16 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import * as React from 'react';
-import ToolBar from './ToolBar';
-import InvitationCard from './InvitationCard';
-import { Container } from '@mui/system';
 import { Paper } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMeetupsPendingAsync } from '../redux/users/thunks';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Container } from '@mui/system';
+import * as React from 'react';
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMeetupsAcceptedAsync, getMeetupsCreatedAsync, getMeetupsDeclinedAsync, getMeetupsPendingAsync } from '../redux/users/thunks';
+import InvitationCard from './InvitationCard';
+import ToolBar from './ToolBar';
 
 export default function Meetups() {
     const dispatch = useDispatch();
@@ -26,8 +26,14 @@ export default function Meetups() {
     );
     const currentUser = useSelector(state => state.usersReducer);
     const invitations = useSelector(state => state.usersReducer.meetupsPending);
+    const acceptedMeetups = useSelector(state => state.usersReducer.meetupsAccepted);
+    const declinedMeetups = useSelector(state => state.usersReducer.meetupsDeclined);
+    const createdMeetups = useSelector(state => state.usersReducer.meetupsCreated);
     useEffect(() => {
         dispatch(getMeetupsPendingAsync(currentUser.email));
+        dispatch(getMeetupsAcceptedAsync(currentUser.email));
+        dispatch(getMeetupsDeclinedAsync(currentUser.email));
+        dispatch(getMeetupsCreatedAsync(currentUser.email));
     }, [dispatch, currentUser.email]);
 
     return (
@@ -37,39 +43,39 @@ export default function Meetups() {
             <ToolBar />
             <Container component="main" sx={{ mb: 4 }}>
                 <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                {/* <Typography component="h1" variant="h5" align="center">
+                    {/* <Typography component="h1" variant="h5" align="center">
                     TODO: Better arrangement when viewing one invitation
                 </Typography> */}
-                <Typography component="h1" variant="h4" align="center">
-                    Pending Invitations
-                </Typography>
-                <Grid
-                    container
-                    spacing={2}
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                {invitations.map(invitation => (
-                    <InvitationCard key={invitation._id} invitation={invitation} />
-                ))}
-                {/* <InvitationCard />
+                    <Typography component="h1" variant="h4" align="center">
+                        Pending Invitations
+                    </Typography>
+                    <Grid
+                        container
+                        spacing={2}
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        {invitations.map(invitation => (
+                            <InvitationCard key={invitation._id} invitation={invitation} />
+                        ))}
+                        {/* <InvitationCard />
                 <InvitationCard />
                 <InvitationCard /> */}
-                </Grid>
-                <Typography component="h1" variant="h4" align="center">
-                    Processed Invitations
-                </Typography>
-                <Grid
-                    container
-                    spacing={2}
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                {/* <InvitationCard />
+                    </Grid>
+                    <Typography component="h1" variant="h4" align="center">
+                        Processed Invitations
+                    </Typography>
+                    <Grid
+                        container
+                        spacing={2}
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        {/* <InvitationCard />
                 <InvitationCard /> */}
-                </Grid>
+                    </Grid>
                 </Paper>
             </Container>
         </ThemeProvider>
