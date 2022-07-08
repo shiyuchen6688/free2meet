9b252dfb159bd4375570b90e4dd8856c1dffec2a
guorunhe <grh20010102@gmail.com>
2022-07-08 06:21:58 -0700

    refresh floating button

--- frontend/src/components/Invitations.js
+++ frontend/src/components/Invitations.js
@@ -1,4 +1,4 @@
-import { Paper } from '@mui/material';
+import { IconButton, Paper } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -11,9 +11,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getMeetupsAcceptedAsync, getMeetupsCreatedAsync, getMeetupsDeclinedAsync, getMeetupsPendingAsync } from '../redux/users/thunks';
 import InvitationCard from './InvitationCard';
 import ToolBar from './ToolBar';
+import Fab from '@mui/material/Fab';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import AddIcon from '@mui/icons-material/Add';
+import Box from '@mui/material/Box';
+import { useNavigate } from "react-router-dom";
 
 export default function Meetups() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
     const theme = React.useMemo(
         () =>
@@ -36,12 +42,34 @@ export default function Meetups() {
         // dispatch(getMeetupsCreatedAsync(currentUser.email));
     }, [dispatch, currentUser.email]);
 
+    const fabStyle = {
+        bottom: theme.spacing(6),
+        right: theme.spacing(6),
+        position: 'fixed'
+    };
+
+    const refresh = () => {
+        console.log('refreshing');
+        dispatch(getMeetupsPendingAsync(currentUser.email));
+        dispatch(getMeetupsAcceptedAsync(currentUser.email));
+        dispatch(getMeetupsDeclinedAsync(currentUser.email));
+        // dispatch(getMeetupsCreatedAsync(currentUser.email));
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
 
             <ToolBar />
             <Container component="main" sx={{ mb: 4 }}>
+                <Box sx={{ '& > :not(style)': { m: 1 } }} style={fabStyle}>
+                    <Fab color="primary" aria-label="fresh" onClick={refresh}>
+                        <AutorenewIcon />
+                    </Fab>
+                    <Fab color="secondary" aria-label="add" onClick={() => navigate("/meetups/new")}>
+                        <AddIcon />
+                    </Fab>
+                </Box>
                 <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                     <Typography component="h5" variant="h5" align="center">
                         Pending Invitations ({invitations.length})
