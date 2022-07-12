10371d03c30275983207802fb1ea4de41798ce65
guorunhe <grh20010102@gmail.com>
2022-07-12 02:55:14 -0700

    chat

--- frontend/src/components/Explore.js
+++ frontend/src/components/Explore.js
@@ -2,6 +2,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { App as SendbirdApp } from "sendbird-uikit";
+import "sendbird-uikit/dist/index.css";
 import ToolBar from './ToolBar';
 
 export default function Explore() {
@@ -15,11 +18,27 @@ export default function Explore() {
             }),
         [prefersDarkMode],
     );
+
+    const APP_ID = "CB108BAB-EB6F-4BA7-A7C5-40E73836AAE1";
+    const currentUser = useSelector(state => state.usersReducer);
+    const USER_ID = currentUser.email;
+    const NICKNAME = currentUser.email;
+    const THEME = prefersDarkMode ? 'dark' : 'light';
+    
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <ToolBar />
-            <h1>TODO: explore public meetups here</h1>
+            <div className="customized-app">
+                <SendbirdApp
+                    appId={APP_ID}
+                    userId={USER_ID}
+                    nickname={NICKNAME}
+                    theme={THEME}
+                    useReaction={true}
+                    useMessageGrouping={true}
+                />
+            </div>
         </ThemeProvider>
     )
 }
