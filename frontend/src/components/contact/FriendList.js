c7310941162df62f57693d95441a8bb5b5d22075
shiyuc <shiyuc@students.cs.ubc.ca>
2022-07-23 12:19:35 -0700

    created comtact folder and tabs to switch

---
+++ frontend/src/components/contact/FriendList.js
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Fab from '@mui/material/Fab';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import AddIcon from '@mui/icons-material/Add';
+import Box from '@mui/material/Box';
+import { useNavigate } from "react-router-dom";
+import { Paper } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import { Avatar } from '@mui/material';
+
+import { getFriendsAsync } from '../../redux/users/thunks';
+
+export default function FriendList() {
+    // get user information // TODO: change to real command
+    const currentUserEmail = useSelector(state => state.usersReducer.email);
+    const friendList = useSelector(state => state.usersReducer.friends);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        dispatch(getFriendsAsync(currentUserEmail));
+    }, [dispatch, currentUserEmail]);
+
+    const refresh = () => {
+        dispatch(getFriendsAsync(currentUserEmail));
+    }
+
+    console.log(friendList)
+
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
+    const fabStyle = {
+        bottom: theme.spacing(6),
+        right: theme.spacing(6),
+        position: 'fixed'
+    };
+
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ '& > :not(style)': { m: 1 } }} style={fabStyle}>
+                <Fab color="primary" aria-label="fresh" onClick={refresh}>
+                    <AutorenewIcon />
+                </Fab>
+                <Fab color="secondary" aria-label="add" onClick={() => navigate("/contact")}>
+                    <AddIcon />
+                </Fab>
+            </Box>
+            <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                <Typography component="h5" variant="h5" align="center">
+                    Friend List ({friendList.length})
+                </Typography>
+                <Grid
+                    container
+                    spacing={2}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    {friendList.map(friend => (
+                        <FriendCard key={friend.email} friend={friend} userEmail={currentUserEmail} state="pending" />
+                    ))}
+                </Grid>
+            </Paper>
+
+
+        </ThemeProvider>
+    )
+
+}
+
+function FriendCard(props) {
+    const { friend, userEmail } = props
+    return (
+        <Box sx={{ minWidth: 275, margin: 5 }}>
+            <Card variant="outlined">
+
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            alt={friend.username}
+                            src={friend.profilePictureLink}
+                        />
+                    }
+                    title={friend.username}
+                />
+
+                <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                        Friend Email: {friend.email}
+                    </Typography>
+                </CardContent>
+
+                {/* # TODO: delete friend */}
+
+            </Card>
+        </Box>
+    )
+}
