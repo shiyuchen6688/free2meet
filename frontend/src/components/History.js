77b38e6b066c089d57b0276f0ebe0d9c9e626f9a
guorunhe <grh20010102@gmail.com>
2022-07-26 03:33:54 -0700

    update ui

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -283,9 +283,6 @@ export default function History() {
             <ToolBar />
             <Container component="main" sx={{ mb: 4 }}>
                 <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                    <Typography component="h1" variant="h4" align="center">
-                        Filter
-                    </Typography>
                     <Grid
                         container
                         spacing={2}
@@ -355,8 +352,8 @@ export default function History() {
                         </>
                     }
                 </Paper>
-                <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
 
+                <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                     <Typography component="h1" variant="h4" align="center">
                         People
                     </Typography>
@@ -371,7 +368,6 @@ export default function History() {
                     </Grid>
                 </Paper>
             </Container>
-
         </ThemeProvider>
     );
 }
