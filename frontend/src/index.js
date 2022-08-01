bc611472ca5a659662b7926af32da463c7264d9c
guorunhe <grh20010102@gmail.com>
2022-07-31 20:18:06 -0700

    clean

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,47 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import {
-  BrowserRouter, Route, Routes
+    BrowserRouter, Route, Routes
 } from "react-router-dom";
 import App from './App';
-import CreateMeetup from './components/CreateMeetup';
 import Contact from './components/contact/Contact';
+import CreateMeetup from './components/CreateMeetup';
 import History from './components/History';
-import './index.css';
 import Invitations from './components/Invitations';
-import Meetups from './components/Meetups';
 import Meetup from './components/Meetup';
-import rootReducer from './redux/reducers';
 import reportWebVitals from './components/reportWebVitals';
 import Signup from './components/Signup';
+import './index.css';
+import rootReducer from './redux/reducers';
 
 const store = configureStore({ reducer: rootReducer });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-
-      <BrowserRouter>
-        <Routes>
-
-          <Route path="/" element={<App />} />
-          {/* <Route path="/meetups" element={<Meetups />} /> */}
-          <Route path="/meetups/:id" element={<Meetup />} />
-          <Route path="/meetups/new" element={<CreateMeetup />} />
-          <Route path="/invitations" element={<Invitations />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signup" element={<Signup />} />
-          {/* <Route path="meetups" element={<TODO />}>
-                <Route path=":meetupId" element={<TODO />} />
-                <Route path="new" element={<TODO />} />
-          </Route> */}
-        </Routes>
-      </BrowserRouter>
-
-    </Provider>
-
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/meetups/:id" element={<Meetup />} />
+                    <Route path="/meetups/new" element={<CreateMeetup />} />
+                    <Route path="/invitations" element={<Invitations />} />
+                    <Route path="/history" element={<History />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 
