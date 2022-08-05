50358512d558a607403810f868ea4d6a8fb66b2f
guorunhe <grh20010102@gmail.com>
2022-08-04 23:36:03 -0700

    update

--- frontend/src/components/contact/FriendRequest.js
+++ frontend/src/components/contact/FriendRequest.js
@@ -11,7 +11,6 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import Fab from '@mui/material/Fab';
 import Grid from '@mui/material/Grid';
