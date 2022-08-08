2c8b6e822bbca575ae9dbcd42dad80cf58d96ae2
mingxingong <57270677+mingxingong@users.noreply.github.com>
2022-08-08 03:00:00 -0700

    added hamburger menu in toolbar, made free2meet clickable

--- frontend/src/components/ToolBar.js
+++ frontend/src/components/ToolBar.js
@@ -15,11 +15,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useNavigate } from "react-router-dom";
 import { logout } from '../redux/users/reducer';
 import UserProfile from './UserProfile';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Drawer } from '@mui/material';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import Link from '@mui/material/Link';
 
 export default function ToolBar() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [open, setOpen] = React.useState(false);
+    const [menu, setMenu] = React.useState(false);
     let currentUser = useSelector(state => state.usersReducer);
 
     const handleClickOpen = () => {
@@ -58,7 +65,42 @@ export default function ToolBar() {
     const handleCloseUserMenu = () => {
         setUser(null);
     };
-
+    
+    const renderUserSettings = () => {
+        return (
+            <Box sx={{ flexGrow: 0, m: 2}}>
+                <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                        <Avatar>{currentUser.username.charAt(0)}</Avatar>
+                    </IconButton>
+                </Tooltip>
+                <Menu
+                    sx={{ mt: '45px' }}
+                    anchorEl={user}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(user)}
+                    onClose={handleCloseUserMenu}
+                >
+                    {settings.map((setting) => (
+                        <MenuItem key={setting.text} onClick={setting.actions}>
+                            <ListItemIcon>
+                                {setting.icon}
+                            </ListItemIcon>
+                            <Typography textAlign="center">{setting.text}</Typography>
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </Box>
+        )
+    }
     return (
         <div>
             {currentUser.username === null ? <Navigate to="/" /> :
@@ -68,47 +110,44 @@ export default function ToolBar() {
                     sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
                 >
                     <Toolbar sx={{ flexWrap: 'nowrap' }}>
-                        <Typography variant="h6" color="inherit" noWrap align='left' sx={{ flexGrow: 1 }}>
-                            free2meet
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, flexWrap: 'wrap', display: { xs: 'flex' } }}>
+                        <Link onClick={() => navigate("/")} color="inherit" underline="none" sx={{ flexGrow: 1 }}>
+                            <Typography variant="h6" color="inherit" noWrap align='left' sx={{ flexGrow: 1 }}>
+                                free2meet
+                            </Typography>
+                        </Link>
+                        <Box sx={{ flexGrow: 1, flexWrap: 'wrap', display: { md: 'flex', xs: 'none'} }}>
                             {pages.map((page) => (
                                 <MenuItem key={page.text} onClick={page.actions} >
                                     <Typography textAlign="center">{page.text}</Typography>
                                 </MenuItem>
                             ))}
                         </Box>
-                        <Box sx={{ flexGrow: 0, m: 2 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar>{currentUser.username.charAt(0)}</Avatar>
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                anchorEl={user}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(user)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting.text} onClick={setting.actions}>
-                                        <ListItemIcon>
-                                            {setting.icon}
-                                        </ListItemIcon>
-                                        <Typography textAlign="center">{setting.text}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
+                        <Box sx={{ flexGrow: 3, display: { md: 'none', xs: 'flex'} }}></Box>
+                        <Box align='right' sx={{ flexGrow: 0, flexWrap: 'wrap', display: { md: 'none', xs: 'flex'} }}>
+                            <IconButton>
+                                <MenuIcon onClick={() => setMenu(true)}/>
+                            </IconButton>
                         </Box>
+                        <Drawer
+                            anchor="right"
+                            open={menu}
+                            onClose={() => setMenu(false)}
+                        >
+                            {renderUserSettings()}
+                            {
+                                pages.map((page) => (
+                                    <ListItem key={page.text}>
+                                        <ListItemButton onClick={page.actions}>
+                                            <ListItemText>{page.text}</ListItemText>
+                                        </ListItemButton>
+                                    </ListItem>
+                                ))
+                            }
+                        </Drawer>
+                        <Box sx={{ display: { md: 'flex', xs: 'none'} }}>
+                            {renderUserSettings()}
+                        </Box>
+                
                         <UserProfile open={open} handleClose={handleClose} currentUser={currentUser} />
                     </Toolbar>
                 </AppBar>}
